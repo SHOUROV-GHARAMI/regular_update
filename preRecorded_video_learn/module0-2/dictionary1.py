ed4151231bb21c48250392bbf7bb03a267c25c3c
SHOUROV-GHARAMI <shourovgharami222@gmail.com>
2025-08-21 21:46:56 +0600

    Revert "Merge remote and local"
    This reverts commit 12b69b245f3d339b3b3075e26cedcc4a9ed565a9, reversing
    changes made to 1b48f9e193cb5f7398dba09f2a522854a917122a.

--- preRecorded_video_learn/module0-2/dictionary1.py
+++ preRecorded_video_learn/module0-2/dictionary1.py
@@ -1,32 +1,32 @@
-a ={"name": "rahim", "karim": 15, 'fahim':28, "rogar": 32, 99:[1,2,3,4] }
-print(type(a))
+a ={"name":"rahim","karim": 15, 'fahim':28 , "rogar": 32, 99:[1,2,3,4] }
+# print(type(a))
 
-for i in a:
-    print(i)
-print("-----------------------------")
-for i in a.values():
-    print(i)
-
-
-print(a.keys(),a.values())
+# for i in a:
+#     print(i)
+# print("-----------------------------")
+# for i in a.values():
+#     print(i)
 
 
+# print(a.keys(),a.values())
 
-for k,v in a.items():
-    print(f"key name: {k}, value: {v}")
+
+
+# for k,v in a.items():
+#     print(f"key name: {k}, value: {v}")
 
 a = [1,2,3,4]
 b = ['Mango', 'Banana', 'Orange', 'Apple' ]
 
 
-print(zip(a,b))
-print(list(zip(a,b)))
-print(dict(zip(a,b)))
+# print(zip(a,b))
+# print(list(zip(a,b)))
+# print(dict(zip(a,b)))
 
-c = zip(a,b)
-print(c)
-print(list(c))
-print(dict(zip(a,b)))
+# c = zip(a,b)
+# print(c)
+# print(list(c))
+# print(dict(zip(a,b)))
 
 
 c = dict(zip(a,b))
