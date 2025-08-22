012743efca72c51e306c85084f209b3ee8a3ab2c
SHOUROV-GHARAMI <shourovgharami222@gmail.com>
2025-08-22 22:32:11 +0600

    oop concept new update

---
+++ bubbleSorting.py
@@ -0,0 +1,17 @@
+array_list = []
+array_size = int(input("Enter array size: "))
+for i in range(array_size):
+    value = int(input("Enter an integer: "))
+    array_list.append(value)
+print(array_list)
+
+def sort_array_list(array_list):
+    for i in range(len(array_list)-1, 0 , -1):
+        for j in range(i):
+            if array_list[j] > array_list[j+1]:
+                temp = array_list[j]
+                array_list[j] = array_list[j+1]
+                array_list[j+1] = temp
+
+sort_array_list(array_list)
+print(array_list)
