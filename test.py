6c7a2ff25a44f9d68fc464ff576cdf2540ed2287
SHOUROV-GHARAMI <shourovgharami222@gmail.com>
2025-12-14 22:27:30 +0600

    new set

---
+++ test.py
@@ -0,0 +1,12 @@
+'''
+a = [1,2,3,4,5,6,7,]
+a[0] = 100
+print(a)
+print(len(a))
+print(a[5])
+print(a[len(a)-2]) 
+'''
+mylist = [ 'apple', 'cherry', 'orange']
+print(list(mylist))
+s = "sdfn.ksjngjkgdskgewkbds,v ,brkv"
+print(list(s))
